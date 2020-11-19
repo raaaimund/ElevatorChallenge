18244adf4e59d901db3d795263c5fc40338331a7
raaaimund <raimund.rittnauer@gmail.com>
2020-11-19 01:25:06 +0100

    added tests for infrastructure

---
+++ ElevatorChallenge.Infrastructure.Tests/Services/Fakes/FakeLogMovementService.cs
@@ -0,0 +1,21 @@
+﻿using ElevatorChallenge.Domain.Entities;
+using ElevatorChallenge.Domain.Enums;
+using ElevatorChallenge.Infrastructure.Services;
+using System;
+
+namespace ElevatorChallenge.Infrastructure.Tests.Services.Fakes
+{
+    public class FakeLogMovementService : BaseLogMovementService
+    {
+        public override void LogMovement(Elevator elevator, ElevatorRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        internal string FakeCreateSymbolForDirection(Direction direction) =>
+            CreateSymbolForDirection(direction);
+
+        internal string FakeCreateMessage(Elevator elevator, ElevatorRequest request) =>
+            CreateMessage(elevator, request);
+    }
+}
